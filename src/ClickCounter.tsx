b4ec83efe6721a9bf7389e76c335d56c1a392a84
ShruthiHanuGowda <shruthi.anugowda@gmail.com>
2021-07-26 19:21:06 +0200

    commiting all

---
+++ src/ClickCounter.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+
+export const ClickCounter = () => {
+  const [count, setCount] = useState(0)
+  return (
+    <div>
+      <h3>Update the count and edit src/App.tsx, state is preserved</h3>
+      <button type="button" onClick={() => setCount((c) => c + 1)}>
+        Count
+        {count}
+      </button>
+    </div>
+  )
+}
